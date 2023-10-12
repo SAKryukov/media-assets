8a76fef4226754d28490cb76cb253b109470c9ee
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2023-10-11 21:09:41 -0400

    Convolution: operational

---
+++ src/Convolution/filters.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+
+    const filters = document.getElementsByTagName("filter");
+    const target = document.querySelector("p");
+    const targetImage = document.querySelector("img");
+    const selectFilter = document.querySelector("section:first-of-type > select");
+    const selectSize = document.querySelector("section:last-of-type > select");
+    const selectImageSize = document.querySelector("figure > select");
+    const colors = document.querySelector("footer");
+
+    for (let index = 0; index < filters.length; ++index) {
+        const filter = filters[index];
+        const option = document.createElement("option");
+        option.textContent = filter.id;
+        selectFilter.appendChild(option);
+    } //loop
+
+    selectFilter.onchange = (event) => {
+        const selectInstance = event.target;
+        const filter = selectInstance.selectedIndex == 0
+            ? null
+            : target.style.filter = `url(#${filters[selectInstance.selectedIndex - 1].id})`;
+        target.style.filter = filter;
+        targetImage.style.filter = filter;            
+    };
+
+    selectFilter.focus();
+
+    selectSize.onchange = (event) => {
+        const selectInstance = event.target;
+        target.style.fontSize = selectInstance.selectedOptions[0].textContent.replace(" ",'');
+    };
+
+    selectImageSize.onchange = (event) => {
+        const selectInstance = event.target;
+        targetImage.style.height = selectInstance.selectedOptions[0].textContent.replace(" ",'');
+    };
+    
+    const colorNames = [`AliceBlue`, `AntiqueWhite`, `Aqua`, `Aquamarine`, `Azure`, `Beige`, `Bisque`, `Black`, `BlanchedAlmond`, `Blue`, `BlueViolet`, `Brown`, `BurlyWood`, `CadetBlue`, `Chartreuse`, `Chocolate`, `Coral`, `CornflowerBlue`, `Cornsilk`, `Crimson`, `Cyan`, `DarkBlue`, `DarkCyan`, `DarkGoldenRod`, `DarkGray`, `DarkGrey`, `DarkGreen`, `DarkKhaki`, `DarkMagenta`, `DarkOliveGreen`, `Darkorange`, `DarkOrchid`, `DarkRed`, `DarkSalmon`, `DarkSeaGreen`, `DarkSlateBlue`, `DarkSlateGray`, `DarkSlateGrey`, `DarkTurquoise`, `DarkViolet`, `DeepPink`, `DeepSkyBlue`, `DimGray`, `DimGrey`, `DodgerBlue`, `FireBrick`, `FloralWhite`, `ForestGreen`, `Fuchsia`, `Gainsboro`, `GhostWhite`, `Gold`, `GoldenRod`, `Gray`, `Grey`, `Green`, `GreenYellow`, `HoneyDew`, `HotPink`, `IndianRed`, `Indigo`, `Ivory`, `Khaki`, `Lavender`, `LavenderBlush`, `LawnGreen`, `LemonChiffon`, `LightBlue`, `LightCoral`, `LightCyan`, `LightGoldenRodYellow`, `LightGray`, `LightGrey`, `LightGreen`, `LightPink`, `LightSalmon`, `LightSeaGreen`, `LightSkyBlue`, `LightSlateGray`, `LightSlateGrey`, `LightSteelBlue`, `LightYellow`, `Lime`, `LimeGreen`, `Linen`, `Magenta`, `Maroon`, `MediumAquaMarine`, `MediumBlue`, `MediumOrchid`, `MediumPurple`, `MediumSeaGreen`, `MediumSlateBlue`, `MediumSpringGreen`, `MediumTurquoise`, `MediumVioletRed`, `MidnightBlue`, `MintCream`, `MistyRose`, `Moccasin`, `NavajoWhite`, `Navy`, `OldLace`, `Olive`, `OliveDrab`, `Orange`, `OrangeRed`, `Orchid`, `PaleGoldenRod`, `PaleGreen`, `PaleTurquoise`, `PaleVioletRed`, `PapayaWhip`, `PeachPuff`, `Peru`, `Pink`, `Plum`, `PowderBlue`, `Purple`, `Red`, `RosyBrown`, `RoyalBlue`, `SaddleBrown`, `Salmon`, `SandyBrown`, `SeaGreen`, `SeaShell`, `Sienna`, `Silver`, `SkyBlue`, `SlateBlue`, `SlateGray`, `SlateGrey`, `Snow`, `SpringGreen`, `SteelBlue`, `Tan`, `Teal`, `Thistle`, `Tomato`, `Turquoise`, `Violet`, `Wheat`, `White`, `WhiteSmoke`, `Yellow`, `YellowGreen`, ];
+
+    for (let index = 0; index < colorNames.length; ++index) {
+        const box = document.createElement("div");
+        box.style.backgroundColor = colorNames[index];
+        box.index = index;
+        box.onclick = (event) => {
+            target.style.color = colorNames[event.target.index];
+        };
+        colors.appendChild(box);
+    } //loop
+
+};
